d09174eb6379b8044e933ee9a65902b928e55bb5
naiqus <zhang@oculavis.de>
2018-12-05 18:20:38 +0100

    Separated all the dconfs from gsettings

--- home/.config/gsettings/exportgsettings.sh
+++ home/.config/gsettings/exportgsettings.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 GSETTINGPATH=$HOME/.homesick/repos/dotfiles/home/.config/gsettings
-dconf dump / > $GSETTINGPATH/gsettings.dconf
+dconf dump /org/gnome/shell/ > $GSETTINGPATH/shell.dconf
+dconf dump /org/gnome/settings-daemon/plugins/ > $GSETTINGPATH/plugins.dconf
+dconf dump /org/gnome/desktop/wm/ > $GSETTINGPATH/wm.dconf
+dconf dump /org/gnome/desktop/interface/ > $GSETTINGPATH/interface.dconf
--- home/.config/gsettings/restoregsettings.sh
+++ home/.config/gsettings/restoregsettings.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 GSETTINGPATH=$HOME/.homesick/repos/dotfiles/home/.config/gsettings
-dconf load / < $GSETTINGPATH/gsettings.dconf
+dconf load /org/gnome/shell/ < $GSETTINGPATH/shell.dconf
+dconf load /org/gnome/settings-daemon/plugins/ < $GSETTINGPATH/plugins.dconf
+dconf load /org/gnome/desktop/wm/ < $GSETTINGPATH/wm.dconf
+dconf load /org/gnome/desktop/interface/ < $GSETTINGPATH/interface.dconf
